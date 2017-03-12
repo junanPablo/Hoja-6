db42de673b7ab253a8cd9e9d05246c6641a282b4
junanPablo <mer13076@uvg.edu.gt>
2017-03-12 17:38:20 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author Juan Pablo Merck
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TiposDesarrolladores miDesa = new TiposDesarrolladores();
+        miDesa.registroDesa();//ingreso de usuarios y que tipo desarrollador es
+        miDesa.ordenamiento();//ordenamiento de desarrolladores en conjuntos
+        
+    }
+    
+}
